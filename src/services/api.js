1ae9c0c01719ca04f3b18697160353abc2d38e21
Alkid Vrushi <vrushialkid@gmail.com>
2025-04-13 12:47:14 +0200

    -changed backend url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://localhost:7046/api';
+const API_URL = 'https://auctionsitebackend.azurewebsites.net//api';
 
 const api = axios.create({
   baseURL: API_URL,
